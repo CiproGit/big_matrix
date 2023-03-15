66d29e44e397cb5ed679e770a98b8787e1f49443
CiproGit <76820530+CiproGit@users.noreply.github.com>
2023-03-15 16:27:08 +0100

    Show in detail the memory occupation of the matrix in bytes and GiB

--- main.cpp
+++ main.cpp
@@ -7,23 +7,32 @@ int main(void) {
 	cout << "*     BIG MATRIX     *\n";
 	cout << "**********************\n" << endl;
 
-	const unsigned int N = 100000000;
+	const unsigned int N = 100000000; // Maximum value for a variable of type unsigned int is 4294967295
+
 	float matrix[N][N];
 
 	cout << "Creating a ";
 	cout << N;
 	cout << " * ";
 	cout << N;
-	cout << " matrix..." << endl;
+	cout << " matrix of floats..." << endl;
 
 	for (unsigned int j = 0; j < N; j++) {
 		for (unsigned int k = 0; k < N; k++) matrix[j][k] = j * k;
 	}
 
-	cout << "Memory occupation: ";
-	cout << (sizeof(float) * N * N) / (1024 * 1024 * 1024);
-	cout << " GB" << endl;
+	cout << "\nWe know that float is 4 bytes and virtual memory limit in a 64 bit machine is 2^64 = 17179869184 GiB.\n" << endl;
 
-	cout << "End" << endl;
+	cout << "Memory occupation of our matrix is ";
+	cout << sizeof(matrix) / (1024 * 1024 * 1024);
+	cout << " GiB, or equivalently ";
+	cout << sizeof(matrix);
+	cout << " B." << endl;
+
+	cout << "Memory occupation of " << N << " * " << N << " floats is ";
+	cout << sizeof(float) * N * N;
+	cout << " B." << endl;
+
+	cout << "\nEnd" << endl;
 	return EXIT_SUCCESS;
 }
